c90bd4ba9dc0eaccef89ea1287e31deed34c6526
JUNHYUK JUNG <kuyhnuj@outlook.com>
2024-02-14 12:13:51 +0900

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Main from "./pages/main";
+import Detail from "./pages/detail";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
